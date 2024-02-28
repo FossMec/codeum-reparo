151a162ef9a8f99b6cd190cf561ddfd186e9a1b0
jzf21 <jozefneelamkavil2003@gmail.com>
2024-02-28 21:20:43 +0530

    cors added

--- apps/server/src/index.js
+++ apps/server/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 var cors = require("cors");
+const port = process.env.PORT || 3001;
 const connectDatabase = require("./db/Database");
 const ErrorHandler = require("./middleware/error");
 dotenv.config();
@@ -10,24 +11,33 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 
-const port = process.env.PORT || 3001;
-
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
+var whitelist = [
+  "http://example1.com",
+  "http://example2.com",
+  process.env.CLIENT_URL,
+];
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+console.log(process.env.CLIENT_URL);
+app.use(cors(corsOptions));
 
 app.use(ErrorHandler);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 connectDatabase();
-let corsOptions = {
-  origin: process.env.CLIENT_URL,
-  credentials: true,
-  optionSuccessStatus: 200,
-};
-console.log(process.env.CLIENT_URL);
-app.use(cors(corsOptions));
+
 const user = require("./controller/user");
 const product = require("./controller/product");
 
